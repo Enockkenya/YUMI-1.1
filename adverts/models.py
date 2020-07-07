c4b29d4c50c834d51beffd8270e7bb448fdef0d0
EnockOMONDI <enockomondike@gmail.com>
2020-07-07 04:10:02 +0300

    #004 DEV [fixed]  advert.count in categories

--- adverts/models.py
+++ adverts/models.py
@@ -5,6 +5,7 @@ from cloudinary.models import CloudinaryField
 
 
 
+from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
     name = models.CharField(max_length=150, db_index=True)
@@ -23,6 +24,8 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def adverts_in_category(self):
+        return self.adverts.all().count()
 
     def get_absolute_url(self):
         return reverse('adverts:adverts_list_by_category', args=[self.slug])
@@ -86,22 +89,20 @@ class Tag(models.Model):
 
 class Advert(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE)
-    location = models.ForeignKey(Location, related_name='location', on_delete=models.CASCADE)
-    option = models.ForeignKey(Option, related_name='option', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='adverts', on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, related_name='adverts', on_delete=models.CASCADE)
+    option = models.ForeignKey(Option, related_name='adverts', on_delete=models.CASCADE)
     item_condition = models.ForeignKey(Item_condition, related_name='item_condition', on_delete=models.CASCADE)
     name = models.CharField(max_length=100, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(max_length=100, db_index=True)
-    price = models.IntegerField(blank=True, null=True)
+    price = models.PositiveIntegerField()
     pub_date = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = CloudinaryField('image', blank=True, null=True)
     available = models.BooleanField(default=True)
     accept_terms = models.BooleanField(default=True)
-    tags = models.ManyToManyField(Tag, default='new' )
-    likes = models.ManyToManyField(User, related_name='advertposts' )
-
+   
 
 
 
@@ -114,9 +115,55 @@ class Advert(models.Model):
 
     def snippet(self):
         return self.description[:50]
+    
+    
+    
 
     def get_absolute_url(self):
         return reverse('adverts:advert_detail',  kwargs={"id":self.id, "slug":self.slug})
 
- 
 
+
+class Like(models.Model):
+    Advert = models.ForeignKey(Advert, related_name="likes", on_delete=models.CASCADE) 
+    user = models.ForeignKey(User,related_name="likes" ,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.advert + '' + 'liked by' + '' + str(self.user)
+
+
+
+class Review(models.Model):
+    """This class creates an ads reviewing odel."""
+
+    reviewer = models.ForeignKey(
+        User,
+        related_name='client_reviews',
+        on_delete=models.CASCADE
+    )
+    reviewee = models.ForeignKey(
+       User,
+        related_name='seller_reviews',
+        on_delete=models.CASCADE
+    )
+    review = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now=True)
+
+    def get_reviewer_name(self):
+        """This method gets the username of the user reveiwing  advert ."""
+        return self.user.username
+
+    def __str__(self):
+        return self.review + '' + ' by' + '' + str(self.reviewer) + '' + 'for' + ' ' +str(self.reviewee)
+   
+
+
+
+    # def get_image(self):
+    #     """This method gets the image of the user rating an article."""
+    #     image_url = CloudinaryImage(str(self.user.profile.image)).build_url(
+    #         width=100, height=150, crop='fill')
+    #     return image_url
+
+    # def __str__(self):
+    #     return "This is rating no: " + str(self.id)
